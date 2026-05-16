faa7679ffb6d1b340389686f40f0fab128ddc081
Luisa Testoni <ra2457040>
2026-05-16 12:12:55 -0300

    Adiciona funções salvar e atualizar cursos

--- app/Http/Controllers/Admin/CursoController.php
+++ app/Http/Controllers/Admin/CursoController.php
@@ -43,4 +43,52 @@ public function excluir($id) {
     // Redireciona para a lista de cursos
 
 }
+
+public function salvar(Request $req)
+{
+    $dados = $req->all();
+
+    if(isset($dados['publicado'])){
+        $dados['publicado'] = 'sim';
+    }else{
+        $dados['publicado'] = 'nao';
+    }
+
+    if($req->hasFile('arquivo')){
+        $imagem = $req->file('arquivo');
+        $num = rand(1111,9999);
+        $dir = "img/cursos/";
+        $ex = $imagem->guessClientExtension();
+        $nomeImagem = "imagem_".$num.".".$ex;
+        $imagem->move($dir,$nomeImagem);
+        $dados['imagem'] = $dir."/".$nomeImagem;
+    }
+
+    Curso::create($dados);
+    return redirect()->route('admin.cursos');
+}
+
+public function atualizar(Request $req, $id)
+{
+    $dados = $req->all();
+
+    if(isset($dados['publicado'])){
+        $dados['publicado'] = 'sim';
+    }else{
+        $dados['publicado'] = 'nao';
+    }
+
+    if($req->hasFile('arquivo')){
+        $imagem = $req->file('arquivo');
+        $num = rand(1111,9999);
+        $dir = "img/cursos/";
+        $ex = $imagem->guessClientExtension();
+        $nomeImagem = "imagem_".$num.".".$ex;
+        $imagem->move($dir,$nomeImagem);
+        $dados['imagem'] = $dir."/".$nomeImagem;
+    }
+
+    Curso::find($id)->update($dados);
+    return redirect()->route('admin.cursos');
+}
 }
